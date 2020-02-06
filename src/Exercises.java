773b3f08843c2778a49bf27cfb3ee7db5f8517c3
Michael Ryan <michael_ryan56@Michaels-MacBook-Air.local>
2020-02-06 14:33:32 -0500

    Added starting constraints for balance & clumps.

--- src/Exercises.java
+++ src/Exercises.java
@@ -76,12 +76,30 @@ public class Exercises {
 	}
 
 	public boolean balance(ArrayList<Integer> numbers) {
+		if (numbers == null || numbers.size() < 2){
+			return false;
+		}
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
 	public int clumps(ArrayList<String> values) {
+		if (values == null) {
+			return -1;
+		}
+
+		boolean containsNull = false;
+		for (int i = 0; i < values.size(); i++){
+			if (values.get(i) == null){
+				containsNull = true;
+			}
+		}
+
+		if (containsNull) {
+			return -1;
+		}
+		
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
